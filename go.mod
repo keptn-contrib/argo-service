8b3a5b742ff7159647fda7435fa1fa8ffa9c9c55
TannerGabriel <gabrieltanner.code@gmail.com>
2022-08-03 11:26:41 +0200

    feat: Reimplement service with go-sdk
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- go.mod
+++ go.mod
@@ -3,22 +3,32 @@ module github.com/keptn-contrib/argo-service
 go 1.17
 
 require (
-	github.com/cloudevents/sdk-go/v2 v2.10.1
-	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/keptn/go-utils v0.17.0
+	github.com/cloudevents/sdk-go/v2 v2.10.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/keptn/go-utils v0.18.1-0.20220802123531-aba76fb153c3
 	gopkg.in/yaml.v3 v3.0.1 // indirect; pin v3.0.1 >= because of CVE-2022-28948
 )
 
+require github.com/sirupsen/logrus v1.8.1
+
 require (
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/cloudevents/sdk-go/observability/opentelemetry/v2 v2.8.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nats-io/nats.go v1.16.0 // indirect
+	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.32.0 // indirect
 	go.opentelemetry.io/otel v1.7.0 // indirect
 	go.opentelemetry.io/otel/metric v0.30.0 // indirect
@@ -26,4 +36,10 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
---
+++ handler/event_handler.go
@@ -0,0 +1,196 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/keptn-contrib/argo-service/pkg/lib/argo"
+	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
+	"github.com/keptn/go-utils/pkg/sdk"
+	"time"
+)
+
+const RealUserTestStrategy = "real-user"
+const MINCANARYWAIT = 0.0
+const MAXCANARYWAIT = 3600.0 // max wait is an hour!
+
+type TestTriggeredExtendedEventData struct {
+	keptnv2.EventData
+
+	Test       TestTriggeredExtendedDetails `json:"test"`
+	Deployment DeploymentDetails            `json:"deployment"`
+}
+
+type DeploymentDetails struct {
+	// DeploymentURILocal contains the local URL
+	DeploymentURIsLocal []string `json:"deploymentURIsLocal"`
+	// DeploymentURIPublic contains the public URL
+	DeploymentURIsPublic []string `json:"deploymentURIsPublic,omitempty"`
+	// DeploymentStrategy defines the used deployment strategy
+	DeploymentStrategy string `json:"deploymentstrategy" jsonschema:"enum=direct,enum=blue_green_service,enum=user_managed"`
+}
+
+type TestTriggeredExtendedDetails struct {
+	// TestStrategy is the testing strategy and is defined in the shipyard
+	TestStrategy       string `json:"teststrategy" jsonschema:"enum=real-user,enum=functional,enum=performance,enum=healthcheck,enum=canarywait"`
+	CanaryWaitDuration string `json:"canarywaitduration"`
+}
+
+type RollbackTriggeredExtendedEventData struct {
+	keptnv2.EventData
+
+	Deployment DeploymentDetails `json:"deployment"`
+}
+
+type TriggeredEventHandler struct {
+}
+
+func NewTriggeredEventHandler() *TriggeredEventHandler {
+	return &TriggeredEventHandler{}
+}
+
+// Execute handles action.triggered events
+func (g *TriggeredEventHandler) Execute(k sdk.IKeptn, event sdk.KeptnEvent) (interface{}, *sdk.Error) {
+	k.Logger().Infof("Handling Event: %s of type %s", event.ID, *event.Type)
+
+	// Now lets see which event we want to handle
+	// Release: We promote for successful evaluations or abort for failed evaluations
+	// Rollback: We will send an abort
+	if *event.Type == keptnv2.GetTriggeredEventType(keptnv2.ReleaseTaskName) {
+		data := &keptnv2.ReleaseTriggeredEventData{}
+		if err := keptnv2.Decode(event.Data, data); err != nil {
+			return nil, &sdk.Error{Err: err, StatusType: keptnv2.StatusErrored, ResultType: keptnv2.ResultFailed, Message: "failed to decode release.triggered event: " + err.Error()}
+		}
+
+		return promote(k, data)
+	} else if *event.Type == keptnv2.GetTriggeredEventType(keptnv2.RollbackTaskName) {
+
+		data := &RollbackTriggeredExtendedEventData{}
+		if err := keptnv2.Decode(event.Data, data); err != nil {
+			return nil, &sdk.Error{Err: err, StatusType: keptnv2.StatusErrored, ResultType: keptnv2.ResultFailed, Message: "failed to decode rollback.triggered event: " + err.Error()}
+		}
+
+		return abort(k, data)
+	} else if *event.Type == keptnv2.GetTriggeredEventType(keptnv2.TestTaskName) {
+		data := &TestTriggeredExtendedEventData{}
+		if err := keptnv2.Decode(event.Data, data); err != nil {
+			return nil, &sdk.Error{Err: err, StatusType: keptnv2.StatusErrored, ResultType: keptnv2.ResultFailed, Message: "failed to decode test.triggered event: " + err.Error()}
+		}
+
+		return testCanaryWait(k, data)
+	}
+
+	return nil, nil
+}
+
+func getActionFinishedEvent(result keptnv2.ResultType, status keptnv2.StatusType, actionTriggeredEvent RollbackTriggeredExtendedEventData, message string) keptnv2.ActionFinishedEventData {
+
+	return keptnv2.ActionFinishedEventData{
+		EventData: keptnv2.EventData{
+			Project: actionTriggeredEvent.Project,
+			Stage:   actionTriggeredEvent.Stage,
+			Service: actionTriggeredEvent.Service,
+			Labels:  actionTriggeredEvent.Labels,
+			Status:  status,
+			Result:  result,
+			Message: message,
+		},
+	}
+}
+
+func getReleaseFinishedEvent(result keptnv2.ResultType, status keptnv2.StatusType, releaseTriggeredEvent keptnv2.ReleaseTriggeredEventData, message string) keptnv2.ReleaseFinishedEventData {
+
+	return keptnv2.ReleaseFinishedEventData{
+		EventData: keptnv2.EventData{
+			Project: releaseTriggeredEvent.Project,
+			Stage:   releaseTriggeredEvent.Stage,
+			Service: releaseTriggeredEvent.Service,
+			Labels:  releaseTriggeredEvent.Labels,
+			Status:  status,
+			Result:  result,
+			Message: message,
+		},
+	}
+}
+
+func getTestFinishedEvent(result keptnv2.ResultType, status keptnv2.StatusType, testTriggeredEvent TestTriggeredExtendedEventData, startedAt time.Time, message string) keptnv2.TestFinishedEventData {
+
+	return keptnv2.TestFinishedEventData{
+		EventData: keptnv2.EventData{
+			Project: testTriggeredEvent.Project,
+			Stage:   testTriggeredEvent.Stage,
+			Service: testTriggeredEvent.Service,
+			Labels:  testTriggeredEvent.Labels,
+			Status:  status,
+			Result:  result,
+			Message: message,
+		},
+		Test: keptnv2.TestFinishedDetails{
+			Start: startedAt.Format(time.RFC3339),
+			End:   time.Now().Format(time.RFC3339),
+		},
+	}
+}
+
+func promote(k sdk.IKeptn, data *keptnv2.ReleaseTriggeredEventData) (interface{}, *sdk.Error) {
+	k.Logger().Info(fmt.Sprintf("Service %s of project %s in stage %s has passed the evaluation",
+		data.Service, data.Project, data.Stage))
+
+	// Promote rollout
+	output, err := argo.Promote(data.Service+"-"+data.Stage, data.Project+"-"+data.Stage)
+	if err != nil {
+		msg := fmt.Sprintf("Error sending rollout promotion event "+
+			"for service %s of project %s and stage %s: %s", data.Service, data.Project,
+			data.Stage, err.Error())
+		return nil, &sdk.Error{Err: err, StatusType: keptnv2.StatusErrored, ResultType: keptnv2.ResultFailed, Message: msg}
+	}
+	k.Logger().Info(output)
+	k.Logger().Infof("Successfully sent promotion event "+
+		"for service %s of project %s and stage %s: %s", data.Service, data.Project, data.Stage, output)
+
+	finishedEventData := getReleaseFinishedEvent(keptnv2.ResultPass, keptnv2.StatusSucceeded, *data, "")
+
+	return finishedEventData, nil
+}
+
+/**
+ * Waits for the canaryWaitSeconds
+ */
+func testCanaryWait(k sdk.IKeptn, data *TestTriggeredExtendedEventData) (interface{}, *sdk.Error) {
+	// lets parse the canarywaitduration
+	waitDuration, err := time.ParseDuration(data.Test.CanaryWaitDuration)
+	if err != nil {
+		return nil, &sdk.Error{Err: err, StatusType: keptnv2.StatusErrored, ResultType: keptnv2.ResultFailed, Message: fmt.Sprintf("Didn't wait because CanaryWaitSeconds of %s is invalid! %s", data.Test.CanaryWaitDuration, err)}
+	}
+
+	// lets wait for the defined seconds
+	if (waitDuration.Seconds() >= MINCANARYWAIT) && (waitDuration.Seconds() < MAXCANARYWAIT) {
+		startedAt := time.Now()
+
+		// lets wait
+		<-time.After(waitDuration)
+
+		finishedEventData := getTestFinishedEvent(keptnv2.ResultPass, keptnv2.StatusSucceeded, *data, startedAt, fmt.Sprintf("Successfully waited for %s seconds!", data.Test.CanaryWaitDuration))
+		return finishedEventData, nil
+	}
+
+	return nil, &sdk.Error{Err: err, StatusType: keptnv2.StatusErrored, ResultType: keptnv2.ResultFailed, Message: fmt.Sprintf("Didn't wait because CanaryWaitSeconds (%s:%f) not within MIN=%f & MAX=%f!", data.Test.CanaryWaitDuration, waitDuration.Seconds(), MINCANARYWAIT, MAXCANARYWAIT)}
+}
+
+func abort(k sdk.IKeptn, data *RollbackTriggeredExtendedEventData) (interface{}, *sdk.Error) {
+	// Abort rollout
+	output, err := argo.Abort(data.Service+"-"+data.Stage, data.Project+"-"+data.Stage)
+	if err != nil {
+		msg := fmt.Sprintf("Error sending abort event for service %s of project %s and stage %s: %s", data.Service, data.Project,
+			data.Stage, err.Error())
+		k.Logger().Error(msg)
+
+		return nil, &sdk.Error{Err: err, StatusType: keptnv2.StatusErrored, ResultType: keptnv2.ResultFailed, Message: msg}
+	}
+
+	k.Logger().Info(output)
+
+	msg := fmt.Sprintf("Successfully sent abort event for service %s of project %s and stage %s: %s", data.Service, data.Project, data.Stage, output)
+	k.Logger().Info(msg)
+
+	finishedEventData := getActionFinishedEvent(keptnv2.ResultPass, keptnv2.StatusSucceeded, *data, msg)
+	return finishedEventData, nil
+}
